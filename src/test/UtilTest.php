9b9b5b91ac40b9b99038222bd8cc1325a86e1d2a
nickolanack <nickblackwell82@gmail.com>
2016-04-07 09:19:13 -0700

    change include

--- src/test/UtilTest.php
+++ src/test/UtilTest.php
@@ -1,28 +1,29 @@
 <?php
 
+class UtilTest extends PHPUnit_Framework_TestCase
+{
 
-class UtilTest extends PHPUnit_Framework_TestCase {
+    public function testParseCoords()
+    {
 
-    public function testParseCoords() {
+        include_once dirname(__DIR__) . '/lib/Util.php';
 
-        include_once '../lib/Util.php';
-        
         // deprecated coordinate storage format
         $c0 = '{"coordinates":[[49.965,-125.20805]]}';
-        
+
         // new coordinate storage format, a single array, with lat, lng and elevation,
         // all coordinates will be updated to this format
         $c1 = '{"coordinates":[50.372,-124.6705,0]}';
-        
+
         $this->assertEquals(array(
             'lat' => 49.965,
-            'lng' => -125.20805
+            'lng' => -125.20805,
         )
-        , Util::ParseCoords($c0));
+            , Util::ParseCoords($c0));
         $this->assertEquals(array(
             'lat' => 50.372,
-            'lng' => -124.6705
+            'lng' => -124.6705,
         )
-        , Util::ParseCoords($c1));
+            , Util::ParseCoords($c1));
     }
 }
